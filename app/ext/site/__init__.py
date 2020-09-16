44b09edc4fe8927b3d73dbc8606007b0e0e89630
Pedro Castilho <pedro@castilho.in>
2020-09-16 09:42:31 -0300

    Handle 404 error and add favicon

--- app/ext/site/__init__.py
+++ app/ext/site/__init__.py
@@ -1,4 +1,9 @@
+from flask import render_template
 from .main import bp
+
+def page_not_found(e):
+    return render_template("error.html", message="Sorry, page not found"), 404
 
 def init_app(app):
     app.register_blueprint(bp)
+    app.register_error_handler(404, page_not_found)
